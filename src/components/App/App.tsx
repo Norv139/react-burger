321ac81d4a7d9f1a844e4ea050bc5e37fc9a5e56
Nikita Babenko <65152247+Norv139@users.noreply.github.com>
2022-04-24 23:26:32 +0300

    Refactoring and bug fixing

---
+++ src/components/App/App.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState, useCallback} from 'react';
+
+// components
+import AppHeader from '../Header/AppHeader'
+import BurgerConstructor from '../BurgerConstructor/BurgerConstructor'
+import BurgerIngredients from '../BurgerIngredients/BurgerIngredients'
+
+//config
+
+import testListData from '../../utils/data';
+
+const url = "https://norma.nomoreparties.space"
+const path = "/api/ingredients"
+
+// style for App.tsx is empty
+
+
+function App() {
+  const [datalistIngredients, setDataListIngredients] = useState(testListData)
+  const [dataIngredients, setDataIngredients] = useState()
+
+  const [success, setSuccess] = useState(false)
+
+
+
+  useEffect(()=>{
+
+    fetch(url+path)
+      .then((response) => {return response.json()})
+      .then( (reqest) => {
+        setDataIngredients(reqest.data); 
+        setSuccess(reqest.success);
+      })
+      .catch((e)=>{console.log(e)})
+
+  }, [])
+
+  return (
+    <div className="App">
+        <AppHeader/>
+        {success &&
+          <main>
+            <BurgerIngredients 
+              dataIngredients={dataIngredients} 
+              openDetals={()=>{console.log('openDetals')}} 
+            />
+            <BurgerConstructor 
+              listIngredients={datalistIngredients} 
+              openDetails={()=>{console.log('openDetails')}} 
+            />
+          </main>
+        }
+          
+    </div>
+  );
+}
+
+export default App;
